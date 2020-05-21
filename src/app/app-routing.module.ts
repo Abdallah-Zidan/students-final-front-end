0c6119f88bb25c533192ab994bbc7a1169311db1
sarahIbraheem <sarahmuhammad3377@gmail.com>
2020-05-21 22:40:46 +0200

    add register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: 'register',
+  component: RegisterComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
