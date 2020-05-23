2dd2a264236b3c08b15dbd90b3ba8b01c0f34fe8
sarahIbraheem <sarahmuhammad3377@gmail.com>
2020-05-23 14:16:19 +0200

    check if user logged in

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faUserFriends,faMoneyBill,faHouseUser,faExchangeAlt,faChalkboardTeacher} from '@fortawesome/free-solid-svg-icons';
-
+import { StorageService } from '../services/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -14,10 +15,18 @@ export class RegisterComponent implements OnInit {
   faExchangeAlt = faExchangeAlt;
   faChalkboardTeacher = faChalkboardTeacher;
   Student=true;
+  public finish = false;
   
-  constructor() {}
+  constructor(private storageService:StorageService,private router:Router) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    const user = this.storageService.getUser('user');
+    if (user) {
+      if (user.token) {
+      this.router.navigate(['']);
+    } 
+  }}
+
   showStudentForm()
   {
     this.Student=true
