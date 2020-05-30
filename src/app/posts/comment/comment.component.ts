fae6ae1e12b47802bcc179ab13d416f98e437d40
Abd Allah Zidan <eng.abdallahzidan@gmail.com>
2020-05-30 11:49:09 +0200

    need fixes

--- src/app/posts/comment/comment.component.ts
+++ src/app/posts/comment/comment.component.ts
@@ -8,16 +8,8 @@ import { PostComment } from 'src/app/education/models/comment.model';
 })
 export class CommentComponent implements OnInit {
   panelOpenState = false;
-<<<<<<< HEAD
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-=======
   @Input() comments: PostComment[];
   constructor() {}
->>>>>>> efaefc3ac5cbdf0c8942c99bcd9c3e433cb07d08
 
   ngOnInit(): void {}
 }
