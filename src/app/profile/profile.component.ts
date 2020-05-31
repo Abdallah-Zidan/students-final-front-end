7a548839fa865cbbbd70cf19ef554f6df4f461de
sarahIbraheem <sarahmuhammad3377@gmail.com>
2020-05-31 20:03:29 +0200

    get user profile data from profile end point and display it

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../services/storage.service';
+import { HttpService } from '../services/http.service';
+
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  data;
+  studyData;
+  constructor(private storagService:StorageService,private httpService:HttpService) {}
+  
 
   ngOnInit(): void {
-  }
+    let user= this.storagService.getItem('user')
+    if(user)
+    {this.httpService.getUser(user).subscribe(
+      result =>{
+        console.log(result)
+        this.data=result.data;
+        console.log(this.data.profile.year)
+      },
+      error =>{
+        console.log(error);}
+     ) }
+      if(user.type=="Student")
+      {
+      
+        this.httpService.getuserDepartment(user).subscribe(
+          result=>{
+           this.studyData=result.data.department_faculties
+          },
+          error=>{console.log(error)}
+        )}
+    
+  }}
 
-}
